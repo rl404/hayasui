93cd777cd5e9be50bba4dc5f86d327dceea08769
axel <axel.rl.404@gmail.com>
2023-09-17 12:14:53 +0700

    ci: enable deployment

--- deployment/terraform/main.tf
+++ deployment/terraform/main.tf
@@ -9,11 +9,11 @@ provider "google" {
   region  = var.gcp_region
 }
 
-# data "google_client_config" "default" {
-# }
+data "google_client_config" "default" {
+}
 
-# provider "kubernetes" {
-#   host                   = "https://${google_container_cluster.cluster.endpoint}"
-#   token                  = data.google_client_config.default.access_token
-#   cluster_ca_certificate = base64decode(google_container_cluster.cluster.master_auth[0].cluster_ca_certificate)
-# }
+provider "kubernetes" {
+  host                   = "https://${google_container_cluster.cluster.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(google_container_cluster.cluster.master_auth[0].cluster_ca_certificate)
+}
